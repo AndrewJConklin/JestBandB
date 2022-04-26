89cf5235b20028f5ffe12fb13b588ecc89202436
aconkli9 <aconkli9@ford.com>
2022-04-26 17:50:55 -0400

    Make tests

--- practice.test.js
+++ practice.test.js
@@ -1,6 +1,16 @@
 const subtract = require(`./index.js`)
 
-test("subract equals the difference of two positive integers", () => {
+test("subtract equals the difference of two positive integers", () => {
     const difference = subtract(3, 2)
     expect(difference).toEqual(1)
+})
+
+test("subtract equals the difference of two negative integers", () => {
+    const difference = subtract(-2, -3)
+    expect(difference).toEqual(1)
+})
+
+test("subtract equals the difference of two floating values", () => {
+    const difference = subtract(0.2, 0.1)
+    expect(difference).toEqual(0.1)
 })
