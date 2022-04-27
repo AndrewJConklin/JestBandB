aed5d38e3eca30a9c605af0ee0ae6ef8bb498c52
aconkli9 <aconkli9@ford.com>
2022-04-27 15:08:23 -0400

    Add tests 2

--- practice.test.js
+++ practice.test.js
@@ -1,20 +1,11 @@
-const { exportAllDeclaration } = require("@babel/types")
-const subtract = require(`./index.js`)
+const subtract = require(`./index`)
 
+test("subtract returns the difference between two postive integers", () => {
+    const diff = subtract(6, 3)
+    expect(diff).toEqual(3)
+})
 
-describe("subtract", () => {
-    it(" equals the difference of two positive integers", () => {
-        const diff = subtract(9, 6)
-        expect(diff).toEqual(3)
-    })
-
-    it(" equals the difference of two negative integers", () => {
-        const diff = subtract(-9, -6)
-        expect(diff).toEqual(-3)
-    })
-
-    it(" equals the difference of two floating values", () => {
-        const diff = subtract(.5, .2)
-        expect(diff).toEqual(.3)
-    })
+test("subtract returns the difference between two negative integers", () => {
+    const diff = subtract(-5, -3)
+    expect(diff).toEqual(-2)
 })
